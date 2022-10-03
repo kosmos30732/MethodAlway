5c546646cf130a9a3bf4b45c92828b277aded47c
Aleksandr Polonnikov <kansk.sasha@live.com>
2022-10-03 20:36:48 +0700

    add polard method

--- MethodAlway/MethodAlway.py
+++ MethodAlway/MethodAlway.py
@@ -1,6 +1,7 @@
 import math
+import random
 
-def alway(n):
+def alway(n : int) -> int:
     d = math.floor(2 * (n ** (1.0 / 3.0)) + 1)
     if d % 2 == 0:
         d += 1
@@ -8,11 +9,12 @@ def alway(n):
     r1 = n % d
     r2 = n % (d - 2)
     s = math.ceil(n ** (0.5))
+    
     while r1 != 0:
         d += 2
         if d > s:
             print("Делителя нет")
-            return
+            return -1
         r = 2 * r1 - r2 + q
         r2 = r1
         r1 = r
@@ -41,8 +43,35 @@ def ferma(n):
             else:
                 continue
 
+def fxc(x,c,n):
+    return ((x*x+c)%n)
+
+def polard(n):
+    times=5
+    a=2
+    b=2
+    c=1
+
+    while(1):
+        a=fxc(a,c,n)
+        b=fxc(fxc(b,c,n),c,n)
+
+        if a==b:
+            if times!=0:
+                times-=1
+                c=random.randint(2,1000)
+                continue
+            else:
+                print("Без результата")
+                return
+        
+        d=math.gcd(abs(a-b),n)
+        if d==1:
+            continue
+        else:
+            return d
 
 
 while(1):
     num = int(input())
-    print(ferma(num))
+    print(polard(num))
